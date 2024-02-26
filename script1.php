1a93afe6d086e1243cbdbce89006621b63853143
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2024-02-26 13:41:44 +0000

    Created

---
+++ script1.php
@@ -0,0 +1,34 @@
+<?php
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // retrieve data
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    //database connect
+    $host = "localhost";
+    $dbusername = "root";
+    $dbpassword = "";
+    $dbname = "auth";
+
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+    if($conn->connect_error){
+        die("Connection failed: ".$conn->connection_error);
+    }
+
+    $query = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
+
+    $result = $conn->query($query);
+
+    if($result->num_rows == 1){
+        header("Location: library.html");
+        exit();
+    }
+    else{
+        header("Location: index.html");
+        exit();
+    }
+
+    $conn->close();
+}
+?>
