c560db127a21e480820d6b89486247a6dfdf26c3
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2024-02-26 15:41:49 +0000

    Updated

--- script1.php
+++ script1.php
@@ -21,11 +21,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     $result = $conn->query($query);
 
     if($result->num_rows == 1){
-        header("Location: library.html");
+        header('Location: library.html';
         exit();
     }
     else{
-        header("Location: index.html");
+        header('Location: index.html');
         exit();
     }
 
