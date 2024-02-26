386c7afc7c317462f3318a2fd55950a00c9fc4c5
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2024-02-26 15:21:07 +0000

    Updated

--- auth.sql
+++ auth.sql
@@ -6,9 +6,11 @@ use auth;
 CREATE TABLE login(
 	username VARCHAR (255),
 	password VARCHAR (255),
+	fullname VARCHAR (255),
+	email VARCHAR (255);
     PRIMARY KEY (username)
 );
 
 INSERT INTO login VALUE(
-"user1", "teamProject"
+"user1", "teamProject", "Jim Jimmer", "jimjimmer@gmail.com"
 );
