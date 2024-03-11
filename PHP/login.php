435f2303540a1d626565a1eb02a39dcb5efb9ee5
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2024-03-11 12:03:33 +0000

    Updated

--- PHP/login.php
+++ PHP/login.php
@@ -12,19 +12,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
     $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
-    if ($conn->connect_error) {
-        die("Connection failed: " . $conn->connect_error);
+    if($conn->connect_error){
+        die("Connection failed: ".$conn->connect_error);
     }
 
-    $query = "SELECT * FROM login WHERE username = '$username' AND password = '$password'";
+    $query = "SELECT * FROM login WHERE username = '$username' AND password = '$password';";
 
     $result = $conn->query($query);
 
-    if ($result->num_rows == 1) {
-        header('Location: /Thoth/library.html');
+    if($result->num_rows == 1){
+        header('Location: library.html');
         exit();
-    } else {
-        header('Location: /Thoth/index.html');
+    }
+    else{
+        echo'<script type="text/javascript">alert("Username and/or password is incorrect");</script>';
         exit();
     }
 }
