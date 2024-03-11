263bdf522663430b8fd15d0093cf98438f837d33
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2024-03-11 12:41:57 +0000

    Updated

--- PHP/login.php
+++ PHP/login.php
@@ -1,5 +1,5 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
+if($_SERVER["REQUEST_METHOD"] == "POST"){
     // retrieve data
     $username = $_POST['username'];
     $password = $_POST['password'];
@@ -7,7 +7,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //database connect
     $host = "localhost";
     $dbusername = "root";
-    $dbpassword = "Timmy2013"; //depends on password
+    $dbpassword = "";
     $dbname = "auth";
 
     $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
@@ -15,17 +15,20 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
     }
-
-    $query = "SELECT * FROM login WHERE username = '$username' AND password = '$password';";
-
-    $result = $conn->query($query);
-
-    if($result->num_rows == 1){
-        header('Location: library.html');
-        exit();
-    }
     else{
-        echo'<script type="text/javascript">alert("Username and/or password is incorrect");</script>';
-        exit();
+        $query = "SELECT * FROM login WHERE username = '$username' AND password = '$password';";
+
+        $result = $conn->query($query);
+
+        if($result->num_rows == 1){
+            header('Location: library.html');
+            $conn->close();
+            exit();
+        }
+        else{
+            echo'<script type="text/javascript">alert("Username and/or password is incorrect");</script>';
+            $conn->close();
+            exit();
+        }
     }
-}
+ }
