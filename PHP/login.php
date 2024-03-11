a782eafa2dd16d18d0855e23893ffb5ac8ec3074
Niamh13 <77692255+Niamh13@users.noreply.github.com>
2024-03-11 12:43:12 +0000

    Updated

--- PHP/login.php
+++ PHP/login.php
@@ -1,13 +1,15 @@
 <?php
 if($_SERVER["REQUEST_METHOD"] == "POST"){
     // retrieve data
+    $name = $_POST['fullName'];
     $username = $_POST['username'];
-    $password = $_POST['password'];
+    $email = $_POST['email'];
+    $password = $_POST['confirmPassword'];
 
     //database connect
     $host = "localhost";
     $dbusername = "root";
-    $dbpassword = "";
+    $dbpassword = ""; //depends on password
     $dbname = "auth";
 
     $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
@@ -15,20 +17,30 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
     }
+
+    $query = "SELECT * FROM login WHERE username = '$username';";
+
+    $result = $conn->query($query);
+
+    if($result->num_rows == 1){
+        echo'<script type="text/javascript">alert("Username has already in-use");</script>';
+        $conn->close();
+        exit();
+    }
     else{
-        $query = "SELECT * FROM login WHERE username = '$username' AND password = '$password';";
-
-        $result = $conn->query($query);
-
-        if($result->num_rows == 1){
-            header('Location: library.html');
+        $query1 = "INSERT INTO login VALUE('$username', '$password', '$name', '$email');";
+        $query1 = "CREATE TABLE '$username'(bookId VARCHAR (255), PRIMARY KEY (bookId));";
+        if($conn->query($query1)===TRUE){
+            echo'<script type="text/javascript">alert("Success");</script>';
+            header('Location: login.html');
             $conn->close();
             exit();
         }
         else{
-            echo'<script type="text/javascript">alert("Username and/or password is incorrect");</script>';
+            echo'<script type="text/javascript">alert("Fail");</script>';
             $conn->close();
             exit();
         }
+        
     }
- }
+}
