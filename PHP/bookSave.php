4987da82d7ffaec366e7013b7bb4943705eb4d3f
joshokeeffecollege <120719225+joshokeeffecollege@users.noreply.github.com>
2024-04-12 19:10:12 +0100

    Update bookSave.php

--- PHP/bookSave.php
+++ PHP/bookSave.php
@@ -2,73 +2,48 @@
 
 session_start();
 
-iclude("connection.php");
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_SESSION['username'])) {
+    // Include login.php to get the username
+    include "login.php";
+    $bookId = $_GET['id'];
+    $username = $_SESSION['username'];
 
-if(isset($_POST['savebutton'])){
-    saveBook();
-}
-
-function saveBook(){
-    // echo variable username
-    include login.php;
-    echo $_SESSION["username"]; 
-    $username = $_SESSION["username"];
-
-    header('Content-Type: application/json');
-
-    $method = $_SERVER['REQUEST_METHOD'];
-    switch($method){
-        case 'POST':
-            $bookId = $_GET["id"];
-            $url = $_COOKIE['url'];
-            $data  = json_decode(file_get_contents($url), true);
-            $title = $data['volumeInfo.title'];
-            $thumbnail = $data['volumeInfo.imageLinks.thumbnail'];
-            $author = $data['volumeInfo.author'];
-            $publisher = $data['volumeInfo.publisher'];
-            $publishDate = $data['volumeInfo.publishedDate'];
-            $pageCount = $data['volumeInfo.pageCount'];
-            $category = $data['volumeInfo.categories'];
-            $rating = $data['volumeInfo.categories'];
-
-            $query1 = "INSERT INTO '$username' (bookId) VALUES ('$bookId')       ";    
-            $query = $conn->prepare('INSERT INTO books (bookId, title, image, author, publisher, publishDate, pageCount, category, rating) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)');
-            if($result->num_rows == 1){
-                echo json_encode(['message' => 'Book already added']);
-            }
-            else{
-                $query->execute([$title, $thumbnail, $author, $publisher, $publishDate, $pageCount, $category, $rating]);
-            }
-
-            echo json_encode(['message' => 'Book added successfully']);
-            if (mysqli_query($conn, $query1)){
-                echo "new record created successfful";
-            }
-            else{
-                echo "Error: ".mysqli_error($conn);
-            }
-            break;
-
-        case 'DELETE':
-            $bookId = $_GET["id"];
-            $query = $conn->prepare('DELETE FROM books WHERE id=?');
-            $query->execute([$bookId]);
-
-            echo json_encode(['message'=> 'Book deleted']);
-            break;
-            
+    $loggIn = $_SESSION['loggIn'];
+    if (loggIn != true) {
+        $loggIn = false;
+        $_SESSION['loggIn'] = $loggIn;
     }
 
+    // Database connection
+    $host = "localhost";
+    $dbusername = "root";
+    $dbpassword = "Timmy2013";
+    $dbname = "auth";
 
+    $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
+
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    else {
+        echo "Unauthorized access.";
+    }
+
+    if (loggIn == true) {
+        // Check if the book ID already exists in the user's table
+        $checkQuery = "SELECT * FROM $username WHERE bookId = '$bookId'";
+        $checkResult = $conn->query($checkQuery);
+
+
+        // Insert the book ID into the user's table
+        $insertQuery = "INSERT INTO $username (bookId) VALUES ('$bookId')";
+    }
+    else if (loggIn == false) {
+        echo '<script type="text/javascript">alert("Please log in"); window.location.href = "Beta2/login.html";</script>';
 }
-
-
-/*
-------------------------------
-----------REFERENCES----------
-------------------------------
-
-[1]: I learned how to save and delete books for the api to the database, as well as echoing variables. URL: https://medium.com/@miladev95/how-to-make-crud-rest-api-in-php-with-mysql-5063ae4cc89
-
-*/
-
+    else {
+        echo '<script type="text/javascript">console.log("ERROR")</script>';
+}
+    $conn->close();
+}
+?>
