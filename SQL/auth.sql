bd7e69bfe93dbf0e18168ee09cd6b9af3d8c838e
joshokeeffecollege <120719225+joshokeeffecollege@users.noreply.github.com>
2024-04-12 19:11:53 +0100

    Update auth.sql

--- SQL/auth.sql
+++ SQL/auth.sql
@@ -14,24 +14,3 @@ CREATE TABLE login
     email    VARCHAR(255),
     PRIMARY KEY (username)
 );
-
-CREATE TABLE books (
-    bookId VARCHAR(255),
-    title VARCHAR(MAX),
-    image VARCHAR(MAX),
-    author VARCHAR(MAX),
-    publisher VARCHAR(MAX),
-    publishDate VARCHAR(MAX),
-    pageCount VARCHAR(MAx),
-    category VARCHAR(MAX),
-    rating VARCHAR(MAX),
-    PRIMARY KEY (bookId)
-);
-
-CREATE TABLE user1 (
-    bookId VARCHAR(255)
-)
-
-INSERT INTO login VALUE(
-'user1', 'teamProject', 'Jim Jimmer', 'jimjimmer@gmail.com'
-);
