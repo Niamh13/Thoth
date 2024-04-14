8ad5b36c7e711f969dc057fefc10de62dcbe8396
joshokeeffecollege <120719225+joshokeeffecollege@users.noreply.github.com>
2024-04-14 17:07:31 +0100

    Update signup.php

--- PHP/signup.php
+++ PHP/signup.php
@@ -8,10 +8,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $passwordC = $_POST['confirmPassword'];
 
     // database connect
-    $host = "localhost";
-    $dbusername = "root";
-    $dbpassword = "Timmy2013";
-    $dbname = "auth";
+    $host = "sql109.infinityfree.com";
+    $dbusername = "if0_35864125";
+    $dbpassword = "superThoth";
+    $dbname = "if0_35864125_auth";
 
     $conn = new mysqli($host, $dbusername, $dbpassword, $dbname);
 
@@ -24,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $result = $conn->query($query);
 
         if ($result->num_rows > 0) {
-            echo '<script type="text/javascript">alert("Username is already in use"); window.location.href = "/Beta 1.1/signup.html";</script>';
+            echo '<script type="text/javascript">alert("Username is already in use"); window.location.href = "/signup.html";</script>';
         } else {
             $query1 = "INSERT INTO login VALUES ('$username', '$password', '$name', '$email')";
             if ($conn->query($query1) === TRUE) {
@@ -34,15 +34,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
                     header('Location: /login.html');
                     exit();
                 } else {
-                    echo '<script type="text/javascript">alert("Fail to create user table");</script>';
+                    echo '<script type="text/javascript">alert("Fail to create user table"); window.location.href = "/signup.html";</script>';
                     header('Location: /signup.html');
                 }
             } else {
-                echo '<script type="text/javascript">alert("Fail to insert user data");</script>';
+                echo '<script type="text/javascript">alert("Fail to insert user data"); window.location.href = "/signup.html";</script>';
             }
         }
     } else {
-        echo '<script type="text/javascript">alert("Passwords entered are not the same");</script>';
+        echo '<script type="text/javascript">alert("Passwords entered are not the same"); window.location.href = "/signup.html";</script>';
     }
     $conn->close();
 }
